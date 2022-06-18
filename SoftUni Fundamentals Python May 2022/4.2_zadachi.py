e1b1bcee453ea7e24c4d3d1778589ff08d4db427
Georgi St. Lukanov <goshiii87@gmail.com>
2022-06-18 20:28:05 +0200

    Create 4.2_zadachi.py

---
+++ SoftUni Fundamentals Python May 2022/4.2_zadachi.py
@@ -0,0 +1,17 @@
+n = int(input())
+left_sum=0
+right_sum=0
+
+for number in range(0 , n*2):
+    current_num = int(input())
+    if number <n:
+        left_sum = left_sum+current_num
+    elif number >= 3:
+        right_sum = right_sum+current_num
+
+if left_sum == right_sum:
+    print("Yes ,sum ="+str(right_sum))
+else:
+    diff = abs(left_sum - right_sum)
+    print("No , diff = "+ str(diff))
+
