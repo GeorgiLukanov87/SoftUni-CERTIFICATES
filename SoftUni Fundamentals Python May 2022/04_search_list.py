66a82af2adebfcb1df219b392de5b2f1d76a8466
Georgi St. Lukanov <goshiii87@gmail.com>
2022-06-21 11:21:53 +0200

    Create 04_search_list.py

---
+++ SoftUni Fundamentals Python May 2022/04_search_list.py
@@ -0,0 +1,15 @@
+n = int(input())
+word = input()
+strings = []
+for i in range(n):
+    current_string = input()
+    strings.append(current_string)
+
+print(strings)
+
+for i in range(len(strings) - 1, -1, -1):
+    element = strings[i]
+    if word not in element:
+        strings.remove(element)
+
+print(strings)
