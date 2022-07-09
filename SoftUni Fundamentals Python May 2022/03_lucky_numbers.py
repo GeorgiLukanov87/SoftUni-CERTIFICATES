c3a257b1fbbd005828ee3a363bc0843656e3d3ec
Georgi St. Lukanov <goshiii87@gmail.com>
2022-07-09 23:29:50 +0200

    Create 03_lucky_numbers.py

---
+++ SoftUni Fundamentals Python May 2022/03_lucky_numbers.py
@@ -0,0 +1,7 @@
+n = int(input())
+for x1 in range(1, 10):
+    for x2 in range(1, 10):
+        for x3 in range(1, 10):
+            for x4 in range(1, 10):
+                if x1 + x2 == x3 + x4 and n % (x1+x2) == 0:
+                    print(f"{x1}{x2}{x3}{x4}", end=" ")
