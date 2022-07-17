d26d4917311acb7d94cc53872059b0412785617f
Georgi St. Lukanov <goshiii87@gmail.com>
2022-07-17 03:52:06 +0200

    Create 02_weekend_or_working_day.py

---
+++ SoftUni Fundamentals Python May 2022/02_weekend_or_working_day.py
@@ -0,0 +1,12 @@
+animal = input()
+
+if animal == "dog":
+    print('mammal')
+elif animal == 'snake':
+    print('reptile')
+elif animal == 'crocodile':
+    print('reptile')
+elif animal == 'tortoise':
+    print('reptile')
+else:
+    print('unknown')
